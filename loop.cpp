47f3fc1dd7327ba06d87b0c0cc6b18d2d61ebb08
LIM LI NING <limlining@graduate.utm.my>
2025-11-13 23:50:14 +0800

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i2=1;i2<=8;i2++){
+    for(int j2=i2;j2>0;j2--)
+    cout<<"+";
+cout<<"\n";
+    }
+  for(int i=0;i<8;i++){
+    for(int j=i;j<8;j++)
+    cout<<"+";
+cout<<"\n";
+  }
+}
